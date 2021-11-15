08b5d9fdf512775e200a7caadf79ba677f9f153e
Etienne Moureton <emoureton@gmail.com>
2021-11-15 22:59:35 +0100

    test

---
+++ .nuxt/components/index.js
@@ -0,0 +1,35 @@
+export { default as Button } from '../../components/Button/Button.vue'
+export { default as Footer } from '../../components/Footer/Footer.vue'
+export { default as Hero } from '../../components/Hero/Hero.vue'
+export { default as Header } from '../../components/Header/Header.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
